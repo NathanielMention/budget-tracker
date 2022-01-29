2ede8a3c60a3e5dcf45d401e6cb06884cbeabfa8
Nathaniel Mention <nathanielmention@gmail.com>
2022-01-29 10:25:11 -0500

    add main component/styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Details from "./components/details/details";
+import Main from "./components/main/main";
 import makeStyles from "./styles";
 
 const app = () => {
@@ -18,8 +19,8 @@ const app = () => {
         <Grid item xs={12} sm={4}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
-          main
+        <Grid item xs={12} sm={3}>
+          <Main />
         </Grid>
         <Grid item xs={12} sm={4}>
           <Details title="Expense" />
---
+++ src/components/main/main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import makeStyles from "./styles";
+
+const main = () => {
+  const classes = makeStyles();
+  return (
+    <Card classname={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered By Speechly" />
+      <CardContent>
+        <Typography align="center" varient="h5">
+          Total Balance $110
+        </Typography>
+        <Typography
+          varient="subtitle1"
+          style={{ lineheight: "1.5em", marginTop: "20px" }}
+        >
+          {/* infocard component */}
+          try saying: add income for $100 in category salary for Monday.
+        </Typography>
+        <Divider />
+        {/* form */}
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spaceing={2}>
+          <Grid item xs={12}>
+            {/* list */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default main;

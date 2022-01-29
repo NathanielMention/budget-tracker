0c8fb4995862d8ee121193dd17ecf47a7dd24e2b
Nathaniel Mention <nathanielmention@gmail.com>
2022-01-29 09:35:19 -0500

    add detail component/styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from "./components/details/details";
+import makeStyles from "./styles";
 
 const app = () => {
-  return <div></div>;
+  const classes = makeStyles();
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default app;
--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
-import useStyles from "./styles";
+import makeStyles from "./styles";
 
-const details = () => {
-  const classes = useStyles();
+const details = ({ title }) => {
+  const classes = makeStyles();
   return (
-    <Card className={classes.income}>
-      <CardHeader title="Income" />
+    <Card className={title === "Income" ? classes.income : classes.expense}>
+      <CardHeader title={title} />
       <CardContent>
         <Typography variant="h5">$50</Typography>
-        <Doughnut data="DATA" />
+        {/* <Doughnut data="DATA" /> */}
       </CardContent>
     </Card>
   );
